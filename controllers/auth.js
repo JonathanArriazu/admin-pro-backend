df94f3c88a8b39559b7ba509c424d6d21c9eb10f
JoniArriazu <joni.arriazu2@gmail.com>
2023-02-16 16:00:59 -0300

    renew token function was added

--- controllers/auth.js
+++ controllers/auth.js
@@ -92,13 +92,25 @@ const googleSignIn = async(req, res = response) => {
             ok: true,
             msg: 'Token de google no es correcto'
         });
-    }
-    
+    }  
+}
 
+const renewToken = async (req, res = response) => {
+
+    const uid = req.uid;
+
+    //Generar el TOKEN - JWT
+    const token = await generarJWT ( uid );
+
+    res.json({
+        ok: true,
+        token
+    })
 }
 
 
 module.exports= {
     login,
-    googleSignIn
+    googleSignIn,
+    renewToken
 }
